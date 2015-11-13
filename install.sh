36c5504c0c723fed89a26332486060e38c5a3ec3
Jonathan Featherstone <jfeatherstone@instructure.com>
2015-11-13 12:20:13 -0700

    Add back black-hole settings for now

--- install.sh
+++ install.sh
@@ -72,12 +72,12 @@ service auth {
 EOF
 
 # config postfix for blackhole
-# postconf -e relayhost=
-# postconf -e relay_transport=relay
-# postconf -e relay_domains=static:ALL
-# postconf -e smtpd_end_of_data_restrictions="check_client_access static:discard"
-# postconf -e smtp_dns_support_level=disabled
-# postconf -e disable_dns_lookups=yes
-# postconf -e in_flow_delay=0
-# postconf -e smtpd_error_sleep_time=0
-# postconf -e smtpd_client_connection_count_limit=0
+postconf -e relayhost=
+postconf -e relay_transport=relay
+postconf -e relay_domains=static:ALL
+postconf -e smtpd_end_of_data_restrictions="check_client_access static:discard"
+postconf -e smtp_dns_support_level=disabled
+postconf -e disable_dns_lookups=yes
+postconf -e in_flow_delay=0
+postconf -e smtpd_error_sleep_time=0
+postconf -e smtpd_client_connection_count_limit=0
